96e0ee07148655a73c1a521e128543a1aacc37c1
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-02-25 16:45:48 -0330

    configuring files to test init.js

--- myapp.js
+++ myapp.js
@@ -1,1 +1,36 @@
+global.DEBUG = true;
+
 const fs = require("fs");
+
+const { initializeApp } = require("./init.js");
+// const { configApp } = require("./config.js");
+// const { tokenApp } = require("./token.js");
+
+const myArgs = process.argv.slice(2);
+
+if (DEBUG) if (myArgs.length >= 1) console.log("the myapp.args: ", myArgs);
+
+switch (myArgs[0]) {
+  case "init":
+  case "i":
+    if (DEBUG) console.log(myArgs[0], " - initialize the app.");
+    initializeApp();
+    break;
+  case "config":
+  case "c":
+    if (DEBUG) console.log(myArgs[0], " - display the configuration file");
+    // configApp();
+    break;
+  case "token":
+  case "t":
+    if (DEBUG) console.log(myArgs[0], " - generate a user token");
+    // tokenApp();
+    break;
+  case "--help":
+  case "--h":
+  default:
+    fs.readFile(__dirname + "/usage.txt", (error, data) => {
+      if (error) throw error;
+      console.log(data.toString());
+    });
+}
