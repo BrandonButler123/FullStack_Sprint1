d5ff820cb5ea54bbf47100b3aa293e19420b9de9
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-02-27 15:13:07 -0330

    logging events
    Adding funcitonaility to logging events

---
+++ logEvents.js
@@ -0,0 +1,45 @@
+// Node.js common core global modules
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+// NPM installed Modules
+const { format, getYear } = require("date-fns");
+const { v4: uuid } = require("uuid"); //guid
+
+const EventEmitter = require("events");
+// class MyEmitter extends EventEmitter {}
+const myEmitter = new EventEmitter();
+
+myEmitter.on("event", async (event, level, message) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${level}\t\t${event}\t\t${message}\t\t${uuid()}`;
+  try {
+    // Include year when managing log folders
+    const currFolder = "logs/" + getYear(new Date());
+    if (!fs.existsSync(path.join(__dirname, "logs/"))) {
+      // if the parent directory logs/ doesn't exist, create it
+      await fsPromises.mkdir(path.join(__dirname, "logs/"));
+      if (!fs.existsSync(path.join(__dirname, currFolder))) {
+        // create the directory for the year ./logs/yyyy
+        await fsPromises.mkdir(path.join(__dirname, currFolder));
+      }
+    } else {
+      if (!fs.existsSync(path.join(__dirname, currFolder))) {
+        // create the directory for the year ./logs/yyyy
+        await fsPromises.mkdir(path.join(__dirname, currFolder));
+      }
+    }
+    // Include todays date in filename
+    if (DEBUG) console.log(logItem);
+    const fileName = `${format(new Date(), "yyyyMMdd")}` + "_http_events.log";
+    await fsPromises.appendFile(
+      path.join(__dirname, currFolder, fileName),
+      logItem + "\n"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = myEmitter;
--- myapp.js
+++ myapp.js
@@ -2,6 +2,8 @@ global.DEBUG = false;
 
 const fs = require("fs");
 
+const myEmitter = require("./logEvents.js");
+
 const { initializeApp } = require("./init.js");
 const { configApp } = require("./config.js");
 const { tokenApp } = require("./token.js");
@@ -15,28 +17,60 @@ switch (myArgs[0]) {
   case "init":
   case "i":
     if (DEBUG) console.log(myArgs[0], " - initialize the app.");
+    myEmitter.emit(
+      "event",
+      "init accessed    ",
+      "INFO",
+      "initializing the app"
+    );
     initializeApp();
     break;
   case "config":
   case "c":
     if (DEBUG) console.log(myArgs[0], " - display the configuration file");
+    myEmitter.emit(
+      "event",
+      "config accessed    ",
+      "INFO",
+      "configuring the app"
+    );
     configApp();
     break;
   case "token":
   case "t":
     if (DEBUG) console.log(myArgs[0], " - generate a user token");
+    myEmitter.emit(
+      "event",
+      "token accessed    ",
+      "INFO",
+      "accessing the token route"
+    );
     tokenApp();
     break;
   case "server":
   case "s":
     if (DEBUG) console.log(myArgs[0], " - start the server");
+    myEmitter.emit("event", "server accessed", "INFO", "starting the server");
     startServer();
     break;
-  case "--help":
-  case "--h":
+  case "help":
+  case "h":
+    myEmitter.emit(
+      "event",
+      "help accessed    ",
+      "INFO",
+      "accessing the help file"
+    );
+    break;
   default:
     fs.readFile(__dirname + "/usage.txt", (error, data) => {
       if (error) throw error;
+      myEmitter.emit(
+        "event",
+        "default accessed",
+        "INFO",
+        "accessing the help file"
+      );
       console.log(data.toString());
     });
 }
